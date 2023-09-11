606a94c1670be2a67b7284815fbc3c74ef917052
Chris Johnston <cjohnston@connected.io>
2023-09-11 14:50:35 -0400

    Adds some documentation to discount

--- elixir/lib/models/discount.ex
+++ elixir/lib/models/discount.ex
@@ -1,3 +1,13 @@
 defmodule Models.Discount do
-  defstruct product: nil, description: "", discount_amount: 0
+  @moduledoc """
+  Represents a discount
+  """
+  defstruct product: nil, description: "", discount_amount: nil
+
+  @doc """
+  Returns a new Discount struct with product, description, and discount amount populated
+  """
+  def initialize(product, description, discount_amount) do
+    %Models.Discount{product: product, description: description, discount_amount: discount_amount}
+  end
 end
