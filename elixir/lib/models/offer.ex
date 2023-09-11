29a6e78a0b375011e862944d7560e96f6046ee2c
Chris Johnston <cjohnston@connected.io>
2023-09-11 14:52:33 -0400

    argument now has a default value of 0

--- elixir/lib/models/offer.ex
+++ elixir/lib/models/offer.ex
@@ -1,5 +1,5 @@
 defmodule Models.Offer do
-  defstruct product: nil, offer_type: nil, argument: nil
+  defstruct product: nil, offer_type: nil, argument: 0
 
   def initialize(offer_type, product, argument) do
     %Models.Offer{offer_type: offer_type, product: product, argument: argument}
