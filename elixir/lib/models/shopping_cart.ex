8fbe5979108c675cd4af516436cf2b6b3cd7d730
Chris Johnston <cjohnston@connected.io>
2023-09-15 15:38:38 -0400

    adds code for x, makes code match original more

--- elixir/lib/models/shopping_cart.ex
+++ elixir/lib/models/shopping_cart.ex
@@ -63,18 +63,26 @@ defmodule Models.ShoppingCart do
 
   defp handle_offer([], receipt, _offers, _catalog), do: receipt
 
-  defp handle_offer([product_quantity | product_quantities], receipt, offers, catalog) do
-    product = product_quantity.product
-    quantity = product_quantity.quantity
-    unit_price = Models.SupermarketCatalog.unit_price(catalog, product)
+  defp handle_offer([pq | product_quantities], receipt, offers, catalog) do
+    p = pq.product
+    quantity = pq.quantity
+    unit_price = Models.SupermarketCatalog.unit_price(catalog, p)
 
     discount =
-      case Map.fetch(offers, product) do
+      case Map.fetch(offers, p) do
         {:ok, offer} ->
+          x =
+            cond do
+              offer.offer_type == :three_for_two -> 3
+              offer.offer_type == :two_for_amount -> 2
+              offer.offer_type == :five_for_amount -> 5
+              true -> 1
+            end
+
           cond do
             offer.offer_type == :ten_percent_discount ->
               Discount.initialize(
-                product,
+                p,
                 "#{offer.argument}% off",
                 quantity * unit_price * offer.argument / 100.0
               )
@@ -82,25 +90,25 @@ defmodule Models.ShoppingCart do
             offer.offer_type == :two_for_amount && quantity >= 2 ->
               total = offer.argument * (quantity / 2) + rem(quantity, 2) * unit_price
               discount_n = unit_price * quantity - total
-              Discount.initialize(product, "2 for #{offer.argument}", discount_n)
+              Discount.initialize(p, "2 for #{offer.argument}", discount_n)
 
             offer.offer_type == :three_for_two && quantity >= 2 ->
-              number_of_x = quantity / 3
+              number_of_x = quantity / x
 
               discount_amount =
                 quantity * unit_price -
                   (number_of_x * 2 * unit_price + rem(quantity, 3) * unit_price)
 
-              Discount.initialize(product, "3 for 2", discount_amount)
+              Discount.initialize(p, "3 for 2", discount_amount)
 
             offer.offer_type == :five_for_amount && quantity >= 5 ->
-              number_of_x = quantity / 5
+              number_of_x = quantity / x
 
               discount_total =
                 unit_price * quantity -
                   (offer.argument * number_of_x + rem(quantity, 5) * unit_price)
 
-              Discount.initialize(product, "5 for #{offer.argument}", discount_total)
+              Discount.initialize(p, "#{x} for #{offer.argument}", discount_total)
 
             true ->
               nil
