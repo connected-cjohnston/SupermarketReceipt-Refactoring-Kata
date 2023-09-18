2a4cdaa8630a7a48fc8900623525e9ac925ac0fc
Chris Johnston <cjohnston@connected.io>
2023-09-18 10:12:28 -0400

    Removes magic atoms, uses "constants" instead

--- elixir/test/models/shopping_cart_test.exs
+++ elixir/test/models/shopping_cart_test.exs
@@ -11,18 +11,25 @@ defmodule Models.ShoppingCartTest do
   end
 
   test "ten percent discount", state do
-    product = Models.Product.initialize("bread", :each)
+    product = Models.Product.initialize("bread", Models.ProductUnit.each())
     catalog = Models.SupermarketCatalog.add_product(state[:catalog], product, 10.00)
     cart = Models.ShoppingCart.add_item_quantity(state[:cart], product, 1)
     teller = Models.Teller.initialize(catalog)
-    teller = Models.Teller.add_special_offer(teller, :ten_percent_discount, product, 10.00)
+
+    teller =
+      Models.Teller.add_special_offer(
+        teller,
+        Models.SpecialOfferType.ten_percent_discount(),
+        product,
+        10.00
+      )
 
     receipt = Models.ShoppingCart.handle_offers(cart, state[:receipt], teller.offers, catalog)
 
     assert receipt == %Models.Receipt{
              discounts: [
                %Models.Discount{
-                 product: %Models.Product{name: "bread", unit: :each},
+                 product: %Models.Product{name: "bread", unit: Models.ProductUnit.each()},
                  description: "10.0% off",
                  discount_amount: 1.0
                }
@@ -32,11 +39,18 @@ defmodule Models.ShoppingCartTest do
   end
 
   test "two for amount", state do
-    product = Models.Product.initialize("bread", :each)
+    product = Models.Product.initialize("bread", Models.ProductUnit.each())
     catalog = Models.SupermarketCatalog.add_product(state[:catalog], product, 1.50)
     cart = Models.ShoppingCart.add_item_quantity(state[:cart], product, 2)
     teller = Models.Teller.initialize(catalog)
-    teller = Models.Teller.add_special_offer(teller, :two_for_amount, product, 2.00)
+
+    teller =
+      Models.Teller.add_special_offer(
+        teller,
+        Models.SpecialOfferType.two_for_amount(),
+        product,
+        2.00
+      )
 
     receipt = Models.ShoppingCart.handle_offers(cart, state[:receipt], teller.offers, catalog)
 
@@ -45,7 +59,7 @@ defmodule Models.ShoppingCartTest do
                %Models.Discount{
                  description: "2 for 2.0",
                  discount_amount: 1.0,
-                 product: %Models.Product{name: "bread", unit: :each}
+                 product: %Models.Product{name: "bread", unit: Models.ProductUnit.each()}
                }
              ],
              items: []
@@ -53,11 +67,18 @@ defmodule Models.ShoppingCartTest do
   end
 
   test "three for two", state do
-    product = Models.Product.initialize("bread", :each)
+    product = Models.Product.initialize("bread", Models.ProductUnit.each())
     catalog = Models.SupermarketCatalog.add_product(state[:catalog], product, 1.50)
     cart = Models.ShoppingCart.add_item_quantity(state[:cart], product, 3)
     teller = Models.Teller.initialize(catalog)
-    teller = Models.Teller.add_special_offer(teller, :three_for_two, product, 1.50)
+
+    teller =
+      Models.Teller.add_special_offer(
+        teller,
+        Models.SpecialOfferType.three_for_two(),
+        product,
+        1.50
+      )
 
     receipt = Models.ShoppingCart.handle_offers(cart, state[:receipt], teller.offers, catalog)
 
@@ -66,7 +87,7 @@ defmodule Models.ShoppingCartTest do
                %Models.Discount{
                  description: "3 for 2",
                  discount_amount: 1.5,
-                 product: %Models.Product{name: "bread", unit: :each}
+                 product: %Models.Product{name: "bread", unit: Models.ProductUnit.each()}
                }
              ],
              items: []
@@ -74,11 +95,18 @@ defmodule Models.ShoppingCartTest do
   end
 
   test "five for amount", state do
-    product = Models.Product.initialize("bread", :each)
+    product = Models.Product.initialize("bread", Models.ProductUnit.each())
     catalog = Models.SupermarketCatalog.add_product(state[:catalog], product, 1.50)
     cart = Models.ShoppingCart.add_item_quantity(state[:cart], product, 5)
     teller = Models.Teller.initialize(catalog)
-    teller = Models.Teller.add_special_offer(teller, :five_for_amount, product, 6.50)
+
+    teller =
+      Models.Teller.add_special_offer(
+        teller,
+        Models.SpecialOfferType.five_for_amount(),
+        product,
+        6.50
+      )
 
     receipt = Models.ShoppingCart.handle_offers(cart, state[:receipt], teller.offers, catalog)
 
@@ -87,7 +115,7 @@ defmodule Models.ShoppingCartTest do
                %Models.Discount{
                  description: "5 for 6.5",
                  discount_amount: 1.0,
-                 product: %Models.Product{name: "bread", unit: :each}
+                 product: %Models.Product{name: "bread", unit: Models.ProductUnit.each()}
                }
              ],
              items: []
